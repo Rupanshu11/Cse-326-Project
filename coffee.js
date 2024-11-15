474ec6e10751d05739b29fe4200708ec87b8a115
Rupanshu Chawla <rupanshuchawla7@gmail.com>
2024-11-15 13:09:52 +0530

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,10 @@
+const navLinks = document.querySelector(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+menuOpenButton.addEventListener("click", ()=>{
+    document.body.classList.toggle("show-mobile-menu")
+});
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+navLinks.forEach(link =>{
+    link.addEventListener("click", ()=>menuOpenButton.click());
+})
